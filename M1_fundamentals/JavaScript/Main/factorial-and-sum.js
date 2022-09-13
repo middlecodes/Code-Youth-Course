e6f439b4a72418df73c81f32c7c4090f41bf3109
Shaneen <shaneencotterell@gmail.com>
2022-09-13 09:04:26 -0400

    factorial and sum js

---
+++ M1_fundamentals/JavaScript/Main/factorial-and-sum.js
@@ -0,0 +1,21 @@
+let sum=0;
+for(let i=1;i<=10;i++){
+	sum = sum + i;
+	console.log(sum,i)	
+}
+console.log("sum of 10 natural numbers:",sum);
+
+let num=5;
+for(let i = num -1; i >= 1; --i){
+	num = num * i;
+    console.log(" i =" + i);
+	console.log(num);
+}
+
+for (let i = 2; i <=10; i+=2){
+console.log(i);
+}
+
+for (let i = 12; i>=0; i-=2){
+    console.log(i)
+}
