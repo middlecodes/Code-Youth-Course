1f8ff4562896884d67545c415cc591f66b3e9798
Shaneen <shaneencotterell@gmail.com>
2022-09-14 09:45:31 -0400

    for loop js

---
+++ M1_fundamentals/JavaScript/Main/for_loop.js
@@ -0,0 +1,9 @@
+for (let i = 2; i <= 10; i+=2){
+console.log(i);
+}
+for (let i = 20; i <= 100; i+=20){
+    console.log(i);
+    }
+    for (let i = 1; i <= 10; i+=2){
+        console.log(i);
+        }
