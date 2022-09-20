487b15b672a0750270593cd4cb1c85d973c59f23
Shaneen <shaneencotterell@gmail.com>
2022-09-20 09:03:20 -0400

    power no power

---
+++ M1_fundamentals/JavaScript/Main/power_no_power.js
@@ -0,0 +1,14 @@
+// 2)
+//Create 2 variables called inputA and inputB. Set them to equal true or false
+//if either inputA or inputB is false console.log("No power")
+//otherwise console.log("Power")
+
+let inputA = true;
+let inputB = false;
+
+if(inputA == false|| inputB==false){
+    console.log("No power");
+}
+else{
+    console.log("Power");
+}
