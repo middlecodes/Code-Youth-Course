7792ec6b9d8bfa4204544bec6069318b35bb5597
Shaneen <shaneencotterell@gmail.com>
2022-09-20 09:03:31 -0400

    my number

---
+++ M1_fundamentals/JavaScript/Main/myNumber.js
@@ -0,0 +1,13 @@
+// 1)
+// Create a variable called myNumber, set it to a number between 0 and 10
+// Create a for loop that runs from 0 to 10, if i equals your number, console.log("My number is " + i)
+
+let myNumber = 3;
+
+for(i = 0; i <=10; i++){
+    myNumber + i;
+}
+   
+    if(i===3){
+   console.log("My number is" + i);
+}
