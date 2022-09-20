9652e018f8a64ae0da26ce778c584d0fc3fcca95
Shaneen <shaneencotterell@gmail.com>
2022-09-20 09:03:40 -0400

    loop practice

---
+++ M1_fundamentals/JavaScript/Main/loop-practice.js
@@ -0,0 +1,9 @@
+// 1)
+// Create a variable called num that equals 0
+// Using a for loop add 5 to num and console.log(num) in the loop
+// the loop should run 20 times
+          
+let num = 0;
+for(num = 0; num<=100; num+=5){
+console.log(num);
+}
