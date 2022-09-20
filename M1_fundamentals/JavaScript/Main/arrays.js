97ddc7880354eb73abd62079e2f958b4ee352751
Shaneen <shaneencotterell@gmail.com>
2022-09-20 09:04:19 -0400

    arrays

---
+++ M1_fundamentals/JavaScript/Main/arrays.js
@@ -0,0 +1,59 @@
+// console.log(marks.length);
+// console.log(marks[marks.length-3]);
+// console.log(marks[marks.length-5]);
+// console.log(marks[marks.length-1]);
+// let marks = [80,90,77,22,95];
+// third one first one last one
+
+// let marks = [80,90,77,22,95];
+// for(i = 0; i<=marks.length; i++){
+// marks[i] = marks[i]+5;
+// console.log(marks[i]);
+// }
+
+// find the total and average of array elements
+
+// // TOTAL
+// let arr = [80,90,77,22,96];
+// sum = 0;
+// for(i = 0; i<arr.length; i++){
+// sum +=arr[i];
+// }
+// console.log(sum);
+
+
+// // AVERAGE
+// let arr = [80,90,77,22,96];
+// sum = 0;
+// for(i = 0; i<arr.length; i++){
+// sum +=arr[i]/5;
+// }
+// console.log(sum/5);
+
+
+// // Question 1: Make an array with 3 different 
+// // data types (string, number,boolean)
+// let data = ["hello",22,true];
+
+
+
+// // Question 2: Create an array with 3 names then 
+// // console.log the array length
+// names = ["beyonce", "kelly", "michelle"];
+// console.log(names.length);
+
+
+// // Question 3: In the previous question, 
+//  // console.log just the first name
+// names = ["beyonce", "kelly", "michelle"];
+// console.log(names[0]);
+
+// Question 4:
+// let arr = ["Hello", "What", "Are", "You"]
+//     console.log() the length of each string in the array
+    let string = ["Hello", "What", "Are", "You"];
+    for(i = 0; i<=)
+    console.log(word.0);
+    console.log(string.1);
+    console.log(string.2);
+    console.log(string.3);
