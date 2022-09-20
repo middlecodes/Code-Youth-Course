378f5c0a6b87548c745ed25f50523be64bd0d8b8
Shaneen <shaneencotterell@gmail.com>
2022-09-20 09:04:00 -0400

    average

---
+++ M1_fundamentals/JavaScript/Main/average.js
@@ -0,0 +1,10 @@
+// sum divided by steps 
+// so total divided by how many numbers
+
+function average(){
+    sum = 0;
+    for(i = 1; i<=10; i++)
+    sum = sum + i;
+    console.log(sum/10);
+}
+average()
