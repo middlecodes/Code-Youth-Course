fc09e23ce3d25752c1854cdecec301291c7c6986
Shaneen <shaneencotterell@gmail.com>
2022-10-01 19:59:09 -0400

    Update objects.js

--- M1_fundamentals/JavaScript/Main/objects.js
+++ M1_fundamentals/JavaScript/Main/objects.js
@@ -97,29 +97,121 @@
 // }
 //     console.log(workPlace)
 
-let students = {
-    info: {
-    iD : 365, 
-    name: "Stewie", 
-    phoneNumber: "4163333333",
-    emailAddress: "stewingstew@gmail.com",
-    marks: 84,
-    },
+// let students = {
+//     info: {
+//     iD : 365, 
+//     name: "Stewie", 
+//     phoneNumber: "4163333333",
+//     emailAddress: "stewingstew@gmail.com",
+//     marks: 84,
+//     },
 
-    address: {
-    city: "Toronto",
-    country: "Canada",
-}
-};
-console.log(students)
-// added 
-students.info.qualifications=true,
-students.address.streetNumber=15;
-console.log(students)
-// deleted
-delete students.info.iD;
-delete students.address.country;
-console.log(students)
+//     address: {
+//     city: "Toronto",
+//     country: "Canada",
+// }
+// };
+// 1. Use for loop to display all keys and properties of student
+// for(let key in students){
 
- 
+// console.log(key, "=>",students[key]);
+// }
 
+// 2. Without using for, display :student John with id(1) and country(US) got (45) marks
+// console.log(students.info.name)
+// console.log(students.info.iD)
+// console.log(students.address.country)
+// console.log(students.info.marks)
+
+// console.log(students)
+// // added 
+// students.info.qualifications=true,
+// students.address.streetNumber=15;
+// console.log(students)
+// // deleted
+// delete students.info.iD;
+// delete students.address.country;
+// console.log(students)
+
+// console.log("keys", Object.keys(students))
+// console.log("values", Object.values(students))
+
+
+// QUESTION BOOKS
+// let books = {
+//     author : "Jim Taylor", 
+//     title: "Marvelous", 
+//     year: "1982",
+//     }
+//     books.publisher="Sail Media",
+
+//     //Display Keys
+//     console.log("keys", Object.keys(books))
+//         //Display Values
+//     console.log("values", Object.values(books))
+//         //Display Properties
+//     console.log("properties", Object.entries(books))
+
+
+
+// Question 1: Create an object countryCap = {Canada: "Ottawa:, America: "Washingoton", Japan: "Tokyo", Australia: "Sidney"}, 
+// then loop through it and console log all the keys (the names of stuff)
+
+// let countryCap = {
+//     Canada: "Ottawa",
+//     America: "Washington",
+//     Japan: "Tokyo",
+//     Australia: "Sydney"
+// }
+// console.log("keys",Object.keys(countryCap));
+
+// for (let items in countryCap){
+//     console.log("Capital",countryCap[items])
+// }
+    
+
+// Loop through the videogames object and console.log() the key and the value
+// If a key's value is undefined don't console.log() it.
+
+// let videoGames = {
+//     action: "Bloodborne",
+//     fighting: "Tekken 7",
+//     racing: undefined,
+//     rhythm: "Osu!",
+// };
+
+// for(let items in action){
+//     console.log("Properties",Object.entries(videoGames))
+
+// if (videoGames.racing == undefined){
+//     console.log();
+//     }
+
+
+// Question 2: Create on object - arcade {game: 'Street Fighter 2', releaseDate: Feb 1991, characters: 
+// ['ryu', 'ken', 'chun-lee']}. Console log the second element inside the characters array in the arcade obj
+
+// Question 3: In the earlier arcade obj, 
+// console log all the items in the characters array using forEach()
+
+
+
+let person = {
+        firstName : "Bob", 
+        lastName: "Fantastic", 
+        phone: "416 733 8555",
+        email: "bob@gmail.com"
+        }
+        let count = 0;
+        for(let key in person){
+        //     count = count+1;
+        //     console.log(key,"=",person[key]);
+        
+        for(let i = 0; i<count; person[key].length;,i++){
+        console.log(match[key][i]);
+        }
+    }
+
+
+//     console.log(count[i])
+//    }
